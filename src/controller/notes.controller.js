071d390fca751a65842b9c57ddf340994f731334
anup <anupc3214@gmail.com>
2024-06-28 14:23:32 +0530

    get notes for student is done

--- src/controller/notes.controller.js
+++ src/controller/notes.controller.js
@@ -5,9 +5,10 @@ import { apiResponse } from "../utils/apiResponse.js";
 import { teachers } from "../model/teacher.model.js";
 import { uploadoncloudnary } from "../utils/cloudnary/cloudnary.js";
 import { notes } from "../model/notes.model.js";
+import { students } from "../model/student.model.js";
 
 const uploadNotes = asyncHandler(async (req, res) => {
-  const { subjectName, chapterNo,standard} = req.body;
+  const { subjectName, chapterNo, standard } = req.body;
 
   if (!req.teacher && !mongoose.Types.ObjectId.isValid(!req.teacher._id)) {
     throw new apiError(401, "user not logged in or invalid user.");
@@ -20,10 +21,16 @@ const uploadNotes = asyncHandler(async (req, res) => {
   }
 
   if (!subjectName && !chapterNo && !standard) {
-    throw new apiError(402, "subjectname and chapterNo and standard  is required");
+    throw new apiError(
+      402,
+      "subjectname and chapterNo and standard  is required"
+    );
   }
 
-  const notePath = req.files?.notes[0].path;
+  const notePath = req.files?.notes[0]?.path;
+  if (!notePath) {
+    throw new apiError(402, "notepath is empty");
+  }
 
   const notesUrl = await uploadoncloudnary(notePath);
 
@@ -36,10 +43,102 @@ const uploadNotes = asyncHandler(async (req, res) => {
     subjectName,
     chapterNo,
     notes: notesUrl.url,
-    standard
+    standard,
   });
 
-  return res.status(200).json(new apiResponse(200, noteFile, "notes uploaded."));
+  if (!noteFile) {
+    throw new apiError(500, "unable to create a note.");
+  }
+
+  const noteData = await notes.aggregate([
+    {
+      $match: { _id: new mongoose.Types.ObjectId(noteFile._id) },
+    },
+    {
+      $lookup: {
+        from: "teachers",
+        localField: "byTeacher",
+        foreignField: "_id",
+        as: "teacherDetails",
+      },
+    },
+    {
+      $unwind: "$teacherDetails",
+    },
+    {
+      $addFields: { teacherName: "$teacherDetails.fullName" },
+    },
+    {
+      $project: {
+        _id: 1,
+        chapterNo: 1,
+        teacherName: 1,
+        standard: 1,
+        subjectName: 1,
+        notes: 1,
+      },
+    },
+  ]);
+
+  if(!noteData){
+    throw new apiError(500,'noteFile is created and note data is unable to create')
+  }
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, noteData[0], "notes uploaded."));
 });
 
-export { uploadNotes };
+const getNotes = asyncHandler(async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.student._id)) {
+    throw new apiError(401, "invalid user");
+  }
+
+  const findStudent = await students.findById(req.student._id);
+
+  if (!findStudent) {
+    throw new apiError(500, "user does not exist");
+  }
+
+  const noteData = await notes.aggregate([
+    {
+      $match: { standard : findStudent.standard },
+    },
+    {
+      $lookup: {
+        from: "teachers",
+        localField: "byTeacher",
+        foreignField: "_id",
+        as: "teacherDetails",
+      },
+    },
+    {
+      $unwind: "$teacherDetails",
+    },
+    {
+      $addFields: { teacherName: "$teacherDetails.fullName" },
+    },
+    {
+      $project: {
+        _id: 1,
+        chapterNo: 1,
+        teacherName: 1,
+        standard: 1,
+        subjectName: 1,
+        notes: 1,
+      },
+    },
+  ]);
+
+  if(!noteData){
+    throw new apiError(500,'note data is unable to retrive')
+  }
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, noteData, "notes recieved."));
+
+
+});
+
+export { uploadNotes ,getNotes};
--- src/routes/notes.router.js
+++ src/routes/notes.router.js
@@ -1,19 +1,24 @@
 import { Router } from "express";
 import { upload } from "../middleware/multer.middlewar.js";
-import { verifyJwtTeacher } from "../middleware/auth.middleware.js";
-import { uploadNotes } from "../controller/notes.controller.js";
+import {
+  verifyJwtStudent,
+  verifyJwtTeacher,
+} from "../middleware/auth.middleware.js";
+import { getNotes, uploadNotes } from "../controller/notes.controller.js";
 
 const router = Router();
 
 router.route("/add-note").post(
-  verifyJwtTeacher,
   upload.fields([
     {
       name: "notes",
       maxCount: 1,
     },
   ]),
+  verifyJwtTeacher,
   uploadNotes
 );
+
+router.route("/get-notes").get(verifyJwtStudent, getNotes);
 
 export default router;
