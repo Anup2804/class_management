14080ae1e6a9599d54d0c0e434675c7c3e6e45f0
anup <anupc3214@gmail.com>
2024-08-08 13:24:22 +0530

    little setting of color and fonts in done

---
+++ class_frontend/lib/utils/constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+// The below is the code of the fontSize
+
+TextStyle fontSmall = const TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle fontRegular = const TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle fontMedium = const TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle fontLarge = const TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle fontExtraLarge = const TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle fontMediumLarge = const TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+);
