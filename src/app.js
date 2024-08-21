9c911ba568db8ee78e940124cccaec5ba08f17f2
anup <anupc3214@gmail.com>
2024-08-21 14:55:00 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(
   })
 );
 
-app.use(express.json({ extended:true, limit: "1mb" })),
+app.use(express.json({  limit: "1mb" })),
   // app.use(express.urlencoded({ extended: true, limit: "1mb" })),
   app.use(express.static("public")),
   app.use(cookieParser());
