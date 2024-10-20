14afa93b460a46f5fac0247b410818a6f507286e
anup <anupc3214@gmail.com>
2024-10-20 19:07:08 +0530

    restart of the frontend

--- class_frontend/lib/main.dart
+++ class_frontend/lib/main.dart
@@ -1,17 +1,4 @@
-import 'package:class_frontend/auth/login.dart';
-import 'package:class_frontend/auth/signup.dart';
-import 'package:class_frontend/pages/otherPages/home.dart';
-import 'package:class_frontend/pages/otherPages/lecture.dart';
-import 'package:class_frontend/pages/otherPages/marks.dart';
-import 'package:class_frontend/pages/otherPages/test.dart';
-import 'package:class_frontend/services/business_logic/student_auth.dart';
-import 'package:class_frontend/services/provider/lecture.provider.dart';
-import 'package:class_frontend/services/provider/student.provider.dart';
-import 'package:class_frontend/services/provider/test.provier.dart';
-import 'package:class_frontend/utils/colors.dart';
-import 'package:class_frontend/utils/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,45 +10,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => StudentProvider(StudentRepository()),
-        ),
-        ChangeNotifierProvider(create: (context) => LectureProvider()),
-        ChangeNotifierProvider(
-          create: (context) => TestProvider(),
-        )
-      ],
-      child: MaterialApp(
+    return  MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          scaffoldBackgroundColor: backGroundColor,
-          textTheme: TextTheme(
-              bodyMedium: fontMedium,
-              bodyLarge: fontLarge,
-              bodySmall: fontSmall,
-              displayLarge: fontExtraLarge,
-              headlineLarge: fontMediumLarge,
-              displayMedium: fontRegular),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-            backgroundColor: buttonColor,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          )),
-        ),
+        
         // home: SignUp(),
-        routes: {
-          '/': (context) => StudentLogin(),
-          '/signup': (context) => SignUp(),
-          '/home': (context) => HomeScreen(),
-          '/lecture': (context) => LectureScreen(),
-          '/test': (context) => TestScreen(),
-          '/marks': (context) => MarksPage()
-        },
-      ),
-    );
+        
+      );
+    
   }
 }
--- class_frontend/lib/utils/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color primaryColor = Color(0xFF1E3A8A);
-const Color backGroundColor = Color.fromARGB(255, 207, 208, 210);
-const Color back1 = Color(0xFF4B5563);
-const Color buttonColor = Color(0xFF14B8A6);
-const Color button1 = Color(0xFFF97316);
-
